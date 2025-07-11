8906da1ce15e3f6390c36d38df12d5024aa73f21
Juan Nícolas <juannicolasdev@gmail.com>
2025-07-10 23:14:24 -0300

    feat: Criando model para as tabelas Autor e Livro

---
+++ src/main/java/exercicio/livros/Model/Livro.java
@@ -0,0 +1,24 @@
+package exercicio.livros.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "livro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String genero;
+    private int paginas;
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private autor autor;
+}
---
+++ src/main/java/exercicio/livros/Model/autor.java
@@ -0,0 +1,23 @@
+package exercicio.livros.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @OneToMany(mappedBy = "autores")
+    private List<Livro> livro;
+}
