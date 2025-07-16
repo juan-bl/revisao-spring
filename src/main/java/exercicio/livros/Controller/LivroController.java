9370641bae8b6f84fbdfe33a5bf61a5cd6ecb57b
Juan Nícolas <juannicolasdev@gmail.com>
2025-07-16 16:27:19 -0300

    doc: Fazendo documentação do Controller do Livros

--- src/main/java/exercicio/livros/Controller/LivroController.java
+++ src/main/java/exercicio/livros/Controller/LivroController.java
@@ -3,6 +3,8 @@ package exercicio.livros.Controller;
 import exercicio.livros.DTO.LivrosDTO;
 import exercicio.livros.Model.LivroModel;
 import exercicio.livros.Service.LivroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,27 +23,32 @@ public class LivroController {
     //fazer o ocntroller
 
     @GetMapping("/listar")
+    @Operation(summary = "Listar livros", description = "Rota que lista todos os livros cadastrados")
     public List<LivroModel> listarLivros() {
         return livroService.listarLivros();
     }
 
     @GetMapping("/listar/{id}")
-    public LivroModel listarPorId(@PathVariable Long id) {
+    @Operation(summary = "Listar livros por ID", description = "Rota que lista todos os livros cadastrados por ID")
+    public LivroModel listarPorId(@Parameter(description = "Extrai o ID da URL") @PathVariable Long id) {
         return livroService.listarPoId(id);
     }
 
     @PostMapping("/criar")
-    public LivrosDTO criarLivro(@RequestBody LivrosDTO livroNovo) {
+    @Operation(summary = "Cadastrar livro", description = "Rota para o cadastro de livro")
+    public LivrosDTO criarLivro(@io.swagger.v3.oas.annotations.parameters.RequestBody @Parameter(description = "Extrai as informações do novo usuario do body") @RequestBody LivrosDTO livroNovo) {
         return livroService.criarLivro(livroNovo);
     }
 
     @PutMapping("/atualizar/{id}")
-    public LivrosDTO atualizarLivro(@PathVariable Long id, @RequestBody LivrosDTO livroAtualizado) {
+    @Operation(summary = "Atualizar livro", description = "Rota para atualizar um livro")
+    public LivrosDTO atualizarLivro(@Parameter(description = "Extrai o ID da URL") @PathVariable Long id, @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Extrai as informações para a atualização usuario do body") @RequestBody LivrosDTO livroAtualizado) {
         return livroService.atualizarLivro(id, livroAtualizado);
     }
 
     @DeleteMapping("/deletar/{id}")
-    public String deletarLivro(@PathVariable Long id) {
+    @Operation(summary = "Deletar livro", description = "Rota para deletar um livro")
+    public String deletarLivro(@Parameter(description = "Extrai o ID da URL") @PathVariable Long id) {
         livroService.deletarLivro(id);
         return "Livro do id " + id + "deletado com sucesso";
     }
