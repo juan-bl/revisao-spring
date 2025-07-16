00e7747424fcf096ecb97faa50870bc429f4171b
Juan Nícolas <juannicolasdev@gmail.com>
2025-07-16 16:11:50 -0300

    doc: Fazendo documentação do Controller do Autor

--- src/main/java/exercicio/livros/Controller/AutorController.java
+++ src/main/java/exercicio/livros/Controller/AutorController.java
@@ -3,6 +3,8 @@ package exercicio.livros.Controller;
 import exercicio.livros.DTO.AutoresDTO;
 import exercicio.livros.Model.AutorModel;
 import exercicio.livros.Service.AutorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,27 +20,32 @@ public class AutorController {
     }
 
     @GetMapping("/listar")
+    @Operation(summary = "Listar autores", description = "Rota que lista todos os autores cadastrados")
     public List<AutorModel> listarAutor() {
         return autorService.listarLivros();
     }
 
     @GetMapping("/listar/{id}")
-    public AutorModel listarPorId(@PathVariable Long id) {
+    @Operation(summary = "Listar autores por ID", description = "Rota que lista todos os autores cadastrados por ID")
+    public AutorModel listarPorId(@Parameter(description = "Extrai o ID da URL") @PathVariable Long id) {
         return autorService.listarPorId(id);
     }
 
     @PostMapping
-    public AutoresDTO criarAutor(@RequestBody AutoresDTO novo) {
+    @Operation(summary = "Cadastrar autor", description = "Rota para o cadastro de autores")
+    public AutoresDTO criarAutor(@io.swagger.v3.oas.annotations.parameters.RequestBody @Parameter(description = "Extrai as informações do novo usuario do body") @RequestBody AutoresDTO novo) {
         return autorService.criarAutor(novo);
     }
 
     @PutMapping("/{id}")
-    public AutoresDTO atualizarAutor(@PathVariable Long id, @RequestBody AutoresDTO novo) {
+    @Operation(summary = "Atualizar autor", description = "Rota para atualizar um autor")
+    public AutoresDTO atualizarAutor(@Parameter(description = "Extrai o ID da URL") @PathVariable Long id, @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Extrai as informações para a atualização usuario do body") @RequestBody AutoresDTO novo) {
         return autorService.atualizarAutor(id, novo);
     }
 
     @DeleteMapping("/{id}")
-    public void deletarAutor(@PathVariable Long id) {
+    @Operation(summary = "Deletar autor", description = "Rota para deletar um autor")
+    public void deletarAutor(@Parameter(description = "Extrai o ID da URL") @PathVariable Long id) {
         autorService.deletarAutor(id);
     }
 }
